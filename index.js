f3108f3a2935cea9b327a302d863dd23c3e21960
KodeKalp <vaibhav@kodekalp.com>
2025-05-21 10:55:31 +0530

    added, deleted

--- index.js
+++ index.js
@@ -5,35 +5,51 @@ const app = express()
 const port = 3111
 
 const books = [
-  { id: 1, title: 'The Alchemist', author: 'Paulo Coelho', year: 1988 },
-  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
-  { id: 3, title: '1984', author: 'George Orwell', year: 1949 }
+    { id: 1, title: 'The Alchemist', author: 'Paulo Coelho', year: 1988 },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+    { id: 3, title: '1984', author: 'George Orwell', year: 1949 }
 ];
 
-app.get('/', (req, res)=>{
-    res.send("Hello NMD")
-})
-
-app.get('/api/books', (req, res)=>{
+app.get('/api/books', (req, res) => {
     res.json(books)
 })
 
-app.get('/api/books/:id', (req, res)=>{
+app.get('/api/books/:id', (req, res) => {
     const bookId = parseInt(req.params.id);
     const book = books.find(book => book.id === bookId)
-    
+
     if (book)
         res.json(book)
     else
-        res.status(404).json({message: "Book Not Found"})
+        res.status(404).json({ message: "Book Not Found" })
 })
 
-app.get('/time', (req, res)=>{
-    const currentTime= new Date().toLocaleDateString();
-    console.log("current Time: ", currentTime)
-    res.send(`Current DAte: ${currentTime}`) 
+app.post('/api/books', (req, res) => {
+   
+
+    const newId = books.length > 0 ? Math.max(...books.map(book => book.id)) + 1 : 1;
+    const newBook = {
+        id: 5,
+        title: "YYAAYA",
+        author: "RETYR",
+        year: 2001
+    }
+    books.push(newBook);
+
+    res.status(201).json(books)
 })
 
-app.listen(port, ()=>{
+app.delete('/api/books/:id', (req, res) => {
+    console.log("delete ")
+    const bookId = parseInt(req.params.id);
+
+    const bookIndex = books.findIndex(book => book.id === bookId)
+
+    const deletedBook = books.splice(bookIndex, 1);
+    res.status(201).json(books)
+})
+
+
+app.listen(port, () => {
     console.log(`server is running on port no. ${port}`)
 })
